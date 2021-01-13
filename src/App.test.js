db2737417273c826c7258688a3bf301f6167928c
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-01-12 16:05:20 -0800

    update test -- PASSING :)

--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,13 @@ import '@testing-library/jest-dom'
 import React from 'react'
 import { act, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-
-import {Quote} from "./components/Quote";
-
+import App from './App';
   
-  test('shows api quote', async () => {
-  
-    act(() => {
-        render(<Quote />);
-        userEvent.click(screen.getByTestId(/getQuote/i));
-    });
-    const quote = await screen.getByTestId(/quote/)
-    // expect(quote).toHaveTextContent("\“\”")
-    expect(quote).toHaveTextContent(/some weird text/);
+describe('App', () => {
+  it('should display quote', async () => {
+    render(<App />)
 
-  });
+    await userEvent.click(screen.getByTestId(/getQuote/i));
+    expect(await screen.findByText(/some weird text/)).toBeInTheDocument()
+  })
+})
