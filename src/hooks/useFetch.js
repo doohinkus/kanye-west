c60ca8d2487c8ebf2e67e8ea6c2636c45532ad84
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-04-24 16:37:47 -0700

    fix loading issue

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,31 +1,33 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export function useFetch(){
+export function useFetch() {
   const [quote, setQuote] = useState("");
-  const [toggle, setToggle] = useState({toggle: false});
-  const endpoint =  "https://api.kanye.rest?format=text";
+  const [toggle, setToggle] = useState({ toggle: false });
+  const endpoint = "https://api.kanye.rest?format=text";
 
   useEffect(() => {
     // COMMON PATTERN USE VARIABLE FOR CLEANUP
     let loaded = false;
-       async function fetchQuote(){
-         const quote = await axios.get(endpoint);
-         const { data } = await quote;
-        //  console.log("QUOTE>>>>>", data);
-           if(!loaded){
-             setQuote(data);
-           }
-     }
-     
-     fetchQuote();
+    async function fetchQuote() {
+      const quote = await axios.get(endpoint);
+      const { data } = await quote;
+      // console.log("QUOTE>>>>>", data.quote);
+      if (!loaded && data) {
+        setQuote(data.quote);
+      }
+      if (!loaded && !data) {
+        setQuote("Trouble fetching quote...");
+      }
+    }
+
+    fetchQuote();
 
     // COMMON PATTERN USE VARIABLE FOR CLEANUP
-     return () => {
-        loaded = true;
-     }
+    return () => {
+      loaded = true;
+    };
     // COMMON PATTERN USE VARIABLE FOR CLEANUP
-
-    },[toggle]);
-    return [{quote, toggle}, setToggle];
+  }, [toggle]);
+  return [{ quote, toggle }, setToggle];
 }
