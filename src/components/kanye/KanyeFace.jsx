d74ac3f5cf10f645545fe0e2284fe59311f98c96
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2024-01-11 11:28:37 -0800

    update styles

--- src/components/kanye/KanyeFace.jsx
+++ src/components/kanye/KanyeFace.jsx
@@ -2,8 +2,9 @@ import "./Kanye.css";
 import kanyeImage from "../../images/kanye-west.png";
 
 export default function KanyeFace({ _class }) {
-  if (_class) {
-    return <img src={kanyeImage} className={`kanye ${_class}`} />;
-  }
-  return <img src={kanyeImage} className="kanye" />;
+  return (
+    <div className={`${_class ? "kanye-wiggle" : ""}`}>
+      <img src={kanyeImage} className={`kanye`} />
+    </div>
+  );
 }
